ea6684ff9868eb3f1d393e07d1c1a61cd25bb736
rachelebarberis <rachele.barberis.12@gmail.com>
2025-02-13 16:10:10 +0100

    esercizio 3

--- BackEnd4/Models/Utente.cs
+++ BackEnd4/Models/Utente.cs
@@ -4,31 +4,44 @@ namespace BackEnd4.Models
 {
     static class Utente
     {
-        public static string User {  get; set; }
+        public static string User { get; set; }
         public static string Password;
         public static string ConfirmPassword { get; set; }
-        public static bool Autentificato => User != null && Password != null;
 
-        public static bool Login (string username, string password, string confirmPassword) 
+      
+        public static bool Autentificato => User != null && Password != null && ConfirmPassword == Password;
+
+
+        public static DateTime? LoginTime { get; private set; }
+
+        public static void Login()
         {
-
-            if (string.IsNullOrWhiteSpace(User))
-            
-                {
-                Console.WriteLine("Inserire qualcosa!");
-                return false;
-            }
-
-            if (Password !=ConfirmPassword)
+            if (Autentificato)
             {
-                Console.WriteLine("Password non valida");
-                    return false;
-            }
 
-            return true; 
-            
-           
+                LoginTime = DateTime.Now;
+                Console.WriteLine($"Login effettuato con successo. Data e ora: {LoginTime}");
+            }
+            else
+            {
+                Console.WriteLine("Errore: Nessun utente autenticato.");
+            }
         }
-         
+        public static void Logout()
+        {
+            if (Autentificato)  
+            {
+           
+                User = null;
+                Password = null;
+                ConfirmPassword = null;
+                LoginTime = null; 
+                Console.WriteLine("Logout effettuato con successo!");
+            }
+            else
+            {
+                Console.WriteLine("Errore: Non sei loggato, impossibile effettuare il logout.");
+            }
+        }
     }
 }
