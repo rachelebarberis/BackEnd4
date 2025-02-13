7bcccc5869c3336828c4ddd3deefefeb1033b75b
rachelebarberis <rachele.barberis.12@gmail.com>
2025-02-13 15:31:41 +0100

    Aggiungere i file di progetto.

---
+++ BackEnd4/Models/Utente.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace BackEnd4.Models
+{
+    static class Utente
+    {
+        public static string User {  get; set; }
+        public static string Password;
+        public static string ConfirmPassword { get; set; }
+        public static bool Autentificato => User != null && Password != null;
+
+        public static bool Login (string username, string password, string confirmPassword) 
+        {
+
+            if (string.IsNullOrWhiteSpace(User))
+            
+                {
+                Console.WriteLine("Inserire qualcosa!");
+                return false;
+            }
+
+            if (Password !=ConfirmPassword)
+            {
+                Console.WriteLine("Password non valida");
+                    return false;
+            }
+
+            return true; 
+            
+           
+        }
+         
+    }
+}
---
+++ BackEnd4/Program.cs
@@ -0,0 +1,18 @@
+﻿using BackEnd4.Models;
+
+
+
+Console.Write("nome utente: ");
+Utente.User= Console.ReadLine();
+
+Console.Write("password: ");
+Utente.Password = Console.ReadLine();
+
+Console.Write("conferma password: ");
+Utente.ConfirmPassword = Console.ReadLine();
+
+if (Utente.Password == Utente.ConfirmPassword)
+{
+    Console.WriteLine("Utente Autentificato");
+}
+
