72bb219751b78f96a4a8f6ecabd9899481a3df4c
rachelebarberis <rachele.barberis.12@gmail.com>
2025-02-13 16:34:32 +0100

    commit finale

--- BackEnd4/Models/Utente.cs
+++ BackEnd4/Models/Utente.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace BackEnd4.Models
+﻿namespace BackEnd4.Models
 {
     static class Utente
     {
@@ -8,18 +6,25 @@ namespace BackEnd4.Models
         public static string Password;
         public static string ConfirmPassword { get; set; }
 
-      
+     
+        private static List<DateTime> accessiStorici = new List<DateTime>();
+
         public static bool Autentificato => User != null && Password != null && ConfirmPassword == Password;
 
-
         public static DateTime? LoginTime { get; private set; }
+
+ 
+        public static List<DateTime> GetStoricoAccessi()
+        {
+            return new List<DateTime>(accessiStorici); 
+        }
 
         public static void Login()
         {
             if (Autentificato)
             {
-
                 LoginTime = DateTime.Now;
+                accessiStorici.Add(LoginTime.Value); 
                 Console.WriteLine($"Login effettuato con successo. Data e ora: {LoginTime}");
             }
             else
@@ -27,15 +32,15 @@ namespace BackEnd4.Models
                 Console.WriteLine("Errore: Nessun utente autenticato.");
             }
         }
+
         public static void Logout()
         {
-            if (Autentificato)  
+            if (Autentificato)
             {
-           
                 User = null;
                 Password = null;
                 ConfirmPassword = null;
-                LoginTime = null; 
+                LoginTime = null;
                 Console.WriteLine("Logout effettuato con successo!");
             }
             else
--- BackEnd4/Program.cs
+++ BackEnd4/Program.cs
@@ -1,49 +1,47 @@
 ﻿using BackEnd4.Models;
 
+Console.Write("Nome utente: ");
+Utente.User = Console.ReadLine();
 
-         
-            Console.Write("Nome utente: ");
-            Utente.User = Console.ReadLine();
+Console.Write("Password: ");
+Utente.Password = Console.ReadLine();
 
-            Console.Write("Password: ");
-            Utente.Password = Console.ReadLine();
+Console.Write("Conferma password: ");
+Utente.ConfirmPassword = Console.ReadLine();
 
-            Console.Write("Conferma password: ");
-            Utente.ConfirmPassword = Console.ReadLine();
+if (Utente.Password == Utente.ConfirmPassword)
+{
+    Utente.Login();
+    Console.WriteLine("Utente autenticato con successo!");
+}
+else
+{
+    Console.WriteLine("Password errata. Riprova.");
+    return;
+}
 
-            if (Utente.Password == Utente.ConfirmPassword)
-            {
-            
-                Utente.Login(); 
-                Console.WriteLine("Utente autenticato con successo!");
-            }
-            else
-            {
-                
-                Console.WriteLine("Password errata. Riprova.");
-                return; 
-            }
+if (Utente.Autentificato)
+{
+    Console.WriteLine("\nStorico degli accessi:");
+    var storicoAccessi = Utente.GetStoricoAccessi();
+    foreach (var accesso in storicoAccessi)
+    {
+        Console.WriteLine(accesso);
+    }
 
-          
-            if (Utente.Autentificato)
-            {
-                Console.WriteLine("Vuoi effettuare il logout? (si/no)");
-                string logoutChoice = Console.ReadLine();
+    Console.WriteLine("\nVuoi effettuare il logout? (si/no)");
+    string logoutChoice = Console.ReadLine();
 
-                if (logoutChoice.ToLower() == "si")
-                {
-        
-                    Utente.Logout();
-                }
-                else
-                {
-                    Console.WriteLine("Non hai effettuato il logout.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Non puoi effettuare il logout senza essere autenticato.");
-            }
-
-     
-
+    if (logoutChoice.ToLower() == "si")
+    {
+        Utente.Logout();
+    }
+    else
+    {
+        Console.WriteLine("Non hai effettuato il logout.");
+    }
+}
+else
+{
+    Console.WriteLine("Non puoi effettuare il logout senza essere autenticato.");
+}
